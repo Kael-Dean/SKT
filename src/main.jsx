517715cbad8f4a8f81b97276dfb11a60b4f74a51
Kael-Dean <pattrakup@outlook.com>
2025-08-17 16:04:52 +0700

    github try11

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
+// ใช้ HashRouter เพื่อให้ทุกเส้นทางทำงานบน GCS/S3 ได้โดยไม่ต้องตั้งค่า redirect
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/skt-front">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )

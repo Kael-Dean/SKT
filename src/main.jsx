91594c1b006773cf1611d9e533add59a80fe2da7
Kael-Dean <pattrakup@outlook.com>
2025-08-17 15:33:44 +0700

    github try6

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/skt-front">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: './', 
+  base: '/skt-front/',   // ต้องตรงกับโฟลเดอร์ที่ deploy ขึ้น GCS
   plugins: [
-    tailwindcss(),
-    react()
+    react(),
+    tailwindcss(),       // อย่าลบออก ถ้าโปรเจกต์คุณใช้ Tailwind
   ],
 })

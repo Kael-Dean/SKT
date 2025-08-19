f6b78d1875de06b3a09c854e28f87e8dfd39154f
Kael-Dean <pattrakup@outlook.com>
2025-08-20 01:10:10 +0700

    สมัคร2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="text-center text-xl font-semibold text-green-600">
-      ✅ หน้าหลักใช้งานได้แล้วเอาให้ได้เถอะ15
+      ✅ หน้าหลักใช้งานได้แล้วเอาให้ได้เถอะ151
     </div>
   )
 }

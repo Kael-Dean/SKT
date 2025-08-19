3ad3de0162b135e9441f00d09546a35cb0256fdc
Kael-Dean <pattrakup@outlook.com>
2025-08-19 18:43:20 +0700

    19

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="text-center text-xl font-semibold text-green-600">
-      ✅ หน้าหลักใช้งานได้แล้วเอาให้ได้เถอะ12
+      ✅ หน้าหลักใช้งานได้แล้วเอาให้ได้เถอะ13
     </div>
   )
 }
